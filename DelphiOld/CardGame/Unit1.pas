4ad99f5e6947636bcb52e403520ced41c2b6da9f
invizor007 <invizor@gmail.com>
2020-12-22 21:59:46 +0300

    Придуманная карточная игра- дополнение

--- DelphiOld/CardGame/Unit1.pas
+++ DelphiOld/CardGame/Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Grids, Buttons, StdCtrls, ExtCtrls, Math;
+  Dialogs, Grids, Buttons, StdCtrls, ExtCtrls;
 
 type
 card=record
@@ -35,7 +35,6 @@ end;
     CheckBox2: TCheckBox;
     Timer1: TTimer;
     SpeedButton8: TSpeedButton;
-    SpeedButton9: TSpeedButton;
     procedure FormCreate(Sender: TObject);
     procedure DrawGrid1DrawCell(Sender: TObject; ACol, ARow: Integer;
       Rect: TRect; State: TGridDrawState);
@@ -64,11 +63,6 @@ end;
     procedure CheckBox1Click(Sender: TObject);
     procedure CheckBox2Click(Sender: TObject);
     procedure SpeedButton8Click(Sender: TObject);
-    procedure Set_Widths;
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure check_win;
-    procedure FormClick(Sender: TObject);
-    procedure SpeedButton9Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -134,7 +128,7 @@ for i:=0 to cg.size do
 can_throw:=ret;
 end;
 //reta[i]=1 if can_throw by i
-function can_cthrow(co:colod):boolean;
+function can_cthrow(co:colod;var reta:array of integer):boolean;
 var res:boolean;
 i:integer;
 begin
@@ -167,7 +161,7 @@ end;
 function cmfh(co:colod):integer;
 var mv,mi,i:integer;//min value and min index
 begin
-mv:=100;
+mv:=100;mi:=-1;
 for i:=0 to co.size-1 do
         if co.a[i].v+10*(co.a[i].m div 3)<mv then
                 begin
@@ -200,19 +194,18 @@ end;
 
 function ss(s:integer):string;//stat string
 begin
-if s=-1 then ss:='Ничего'
-//else if s=-2 then ss:='do hod'
-else if s=0 then ss:='Сброс'
-else if s=1 then ss:='Отбить'
-else if s=2 then ss:='Забрать'
-else if s=3 then ss:='Первый ход'
+if s=-1 then result:='  '//nothing
+else if s=0 then result:='Сброс'
+else if s=1 then result:='Отбить'
+else if s=2 then result:='Забрать'
+else if s=3 then result:='Первый ход'
+else result:='  ';
 end;
 //moving of card from place fp of cf to end of ct
 procedure cmove(var cf,ct:colod;fp:integer);
 var ca:card;
 i:integer;
 begin
-if cf.size=0 then exit;
 ca.v:=cf.a[fp].v;
 ca.m:=cf.a[fp].m;
 cf.size:=cf.size-1;
@@ -231,7 +224,7 @@ begin
 while (cf.size<>0) do
         cmove(cf,ct,0);
 end;
-//obratnoe movanie,from colod from(not) last to colod to
+//obreratnoe movanie,from colod from(not) last to colod to
 procedure obcmove(var cfl,cfnl,ct:colod);
 var i,j:integer;
 begin
@@ -257,14 +250,14 @@ end;
 //dobrat karty do nedostayushih count
 procedure taketo7(var cf,ct:colod;count:integer);
 begin
-while (ct.size<count)and(cf.size>0) do
+while (ct.size<count)and(ct.size>0) do
         cmove(cf,ct,0);
 end;
 //outinf
 procedure TForm1.outinf;
 begin
 Caption:='Кон '+inttostr(kon)+'.  Ход='+inttostr(hod)+'.  Игрок='+
-        inttostr(currig)+'.   Осталось карт:'+inttostr(c.size)+' '+inttostr(c1.size) +' '+inttostr(c2.size);
+        inttostr(currig);
 Label1.Caption:=ss(stat)+' '+inttostr(eop);
 end;
 
@@ -272,8 +265,8 @@ procedure TForm1.out_colod(Dg:TDrawGrid;co:colod);
 var i:integer;
 begin
 Dg.ColCount:=co.size;
-Dg.Width:=Dg.DefaultColWidth*min(7,co.size)+9;//??
-Dg.Repaint;
+Dg.Width:=Dg.DefaultColWidth*co.size+9;//??
+for i:=0 to co.size do Dg.Repaint;
 end;
 
 procedure TForm1.first_hod(fh:boolean);
@@ -295,12 +288,10 @@ var count,r:integer;
 i:integer;
 begin
 Bmp:=TBitmap.Create;
-bmp.Width:=DrawGrid1.DefaultColWidth;
-bmp.Height:=DrawGrid1.DefaultRowHeight;
+bmp.Width:=40;bmp.Height:=40;
 randomize;//tus
 count:=0;
 for i:=0 to 1 do comp[i]:=0;
-for i:=0 to 35 do full[i]:=0;
 while count<36 do
     begin
     repeat
@@ -319,33 +310,22 @@ for i:=0 to 6 do
         c1.a[i].m:=c.a[22+i].m;
         c2.a[i].v:=c.a[29+i].v;
         c2.a[i].m:=c.a[29+i].m;
-        end;     
-//DrawGrid1.ColCount:=c1.size;
-//DrawGrid2.ColCount:=c2.size;
-//DrawGrid3.ColCount:=cg.size;
-//DrawGrid4.ColCount:=ce.size;
-set_widths;
+        end;
 first_hod(true);
-outinf;        
+outinf;
 end;
 //DRAWCELL
 procedure TForm1.DrawGrid1DrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState);
 begin
-if acol<c1.size then
-        Bmp.LoadFromFile(to_fs(c1.a[acol]))
-else
-        Bmp.Canvas.FillRect(Classes.Rect(0,0,TDrawGrid(Sender).DefaultColWidth,TDrawGrid(Sender).DefaultRowHeight));
+Bmp.LoadFromFile(to_fs(c1.a[acol]));
 DrawGrid1.Canvas.Draw(rect.Left,rect.Top,bmp);
 end;
 
 procedure TForm1.DrawGrid2DrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState);
 begin
-if acol<c2.size then
-        Bmp.LoadFromFile(to_fs(c2.a[acol]))
-else
-        Bmp.Canvas.FillRect(Classes.Rect(0,0,TDrawGrid(Sender).DefaultColWidth,TDrawGrid(Sender).DefaultRowHeight));
+Bmp.LoadFromFile(to_fs(c2.a[acol]));
 DrawGrid2.Canvas.Draw(rect.Left,rect.Top,bmp);
 end;
 
@@ -353,20 +333,20 @@ procedure TForm1.DrawGrid3DrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState);
 begin
 if (cg.size>0)and(acol<cg.size) then
-        Bmp.LoadFromFile(to_fs(cg.a[acol]))
-else
-        Bmp.Canvas.FillRect(Classes.Rect(0,0,TDrawGrid(Sender).DefaultColWidth,TDrawGrid(Sender).DefaultRowHeight));
-DrawGrid3.Canvas.Draw(rect.Left,rect.Top,bmp);
+        begin
+        Bmp.LoadFromFile(to_fs(cg.a[acol]));
+        DrawGrid3.Canvas.Draw(rect.Left,rect.Top,bmp);
+        end;
 end;
 
 procedure TForm1.DrawGrid4DrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState);
 begin
 if (ce.size>0)and(acol<ce.size) then
-        Bmp.LoadFromFile(to_fs(ce.a[acol]))
-else
-        Bmp.Canvas.FillRect(Classes.Rect(0,0,TDrawGrid(Sender).DefaultColWidth,TDrawGrid(Sender).DefaultRowHeight));
-DrawGrid4.Canvas.Draw(rect.Left,rect.Top,bmp);
+        begin
+        Bmp.LoadFromFile(to_fs(ce.a[acol]));
+        DrawGrid4.Canvas.Draw(rect.Left,rect.Top,bmp);
+        end;
 end;
 //DOUBLECLICK
 procedure TForm1.DrawGrid1DblClick(Sender: TObject);
@@ -562,10 +542,10 @@ if eop=1 then
                 eop:=0;SpeedButton5.Visible:=false;
                 out_colod(DrawGrid1,c1);
                 out_colod(DrawGrid2,c2);
-                set_widths;
+                out_colod(DrawGrid3,ci1);
+                out_colod(DrawGrid4,ci2);
                 end;
         end;
-check_win;
 end;
 //SAVELOAD
 procedure TForm1.SpeedButton6Click(Sender: TObject);
@@ -586,30 +566,6 @@ if saveDialog1.Execute then
         end;
 end;
 
-procedure TForm1.Set_Widths;
-var i:integer;
-begin
-        DrawGrid1.ColCount:=c1.size;
-        DrawGrid1.Width:=(DrawGrid1.DefaultColWidth+2)*min(7,c1.size)+9;
-        for i:=0 to c1.size-1 do
-                DrawGrid1DrawCell(DrawGrid1,i,0,DrawGrid1.CellRect(i,0),[]);
-
-        DrawGrid2.ColCount:=c2.size;
-        DrawGrid2.Width:=(DrawGrid2.DefaultColWidth+2)*min(7,c2.size)+9;
-        for i:=0 to c2.size-1 do
-                DrawGrid2DrawCell(DrawGrid2,i,0,DrawGrid2.CellRect(i,0),[]);
-
-        DrawGrid3.ColCount:=cg.size;
-        DrawGrid3.Width:=(DrawGrid3.DefaultColWidth+2)*min(7,cg.size)+9;
-        for i:=0 to cg.size-1 do
-                DrawGrid3DrawCell(DrawGrid3,i,0,DrawGrid3.CellRect(i,0),[]);
-
-        DrawGrid4.ColCount:=ce.size;
-        DrawGrid4.Width:=(DrawGrid4.DefaultColWidth+2)*min(7,ce.size)+9;
-        for i:=0 to ce.size-1 do
-                DrawGrid4DrawCell(DrawGrid4,i,0,DrawGrid4.CellRect(i,0),[]);
-end;
-
 procedure TForm1.SpeedButton7Click(Sender: TObject);
 var i:integer;
 begin
@@ -627,7 +583,26 @@ if OpenDialog1.Execute then
         loadcolod(ce);
         closefile(f);
 
-        Set_Widths;
+        DrawGrid1.ColCount:=c1.size;
+        DrawGrid1.Width:=DrawGrid1.DefaultColWidth*c1.size+9;
+        for i:=0 to c1.size-1 do
+                DrawGrid1DrawCell(DrawGrid1,i,0,DrawGrid1.CellRect(i,0),[]);
+
+        DrawGrid2.ColCount:=c2.size;
+        DrawGrid2.Width:=DrawGrid2.DefaultColWidth*c2.size+9;
+        for i:=0 to c2.size-1 do
+                DrawGrid2DrawCell(DrawGrid2,i,0,DrawGrid2.CellRect(i,0),[]);
+
+        DrawGrid3.ColCount:=cg.size;
+        DrawGrid3.Width:=DrawGrid3.DefaultColWidth*cg.size+9;
+        for i:=0 to cg.size-1 do
+                DrawGrid3DrawCell(DrawGrid3,i,0,DrawGrid3.CellRect(i,0),[]);
+
+        DrawGrid4.ColCount:=ce.size;
+        DrawGrid4.Width:=DrawGrid4.DefaultColWidth*ce.size+9;
+        for i:=0 to ce.size-1 do
+                DrawGrid4DrawCell(DrawGrid4,i,0,DrawGrid4.CellRect(i,0),[]);
+
         if hod=0 then first_hod(true)
         else first_hod(false);
         if eop=1 then SpeedButton5.Visible:=true;
@@ -663,52 +638,46 @@ if comp[currig-1]=1 then
                         if currig=1 then
                                 begin  //sbros bez kozyrey
 
-                                if can_cthrow(c1)=true then
-                                        begin    
+                                if can_cthrow(c1,ret)=true then
+                                        begin
                                         SpeedButton1Click(SpeedButton1);
-                                        for i:=c1.size-1 downto 0 do if reta[i]=1 then
-//                                        while can_cthrow(c1) do
+                                        for i:=0 to c1.size-1 do if reta[i]=1 then
                                                 begin
                                                 DrawGrid1.Col:=i;
                                                 DrawGrid1DblClick(DrawGrid1);
                                                 end;
 
                                         end else //otbit
-                                if can_cbeat(cg.a[cg.size-1],c1,ret)=true then
+                                if can_cbeat(c1.a[c1.size-1],cg,ret)=true then
                                         begin
                                         SpeedButton2Click(SpeedButton2);
                                         DrawGrid1.Col:=ret;
-                                        DrawGrid1DblClick(DrawGrid1);
                                         end else//sbros kozyrey
                                 if can_cthrow2(c1,ret)=true then
                                         begin
-                                        SpeedButton1Click(SpeedButton1);
                                         DrawGrid1.Col:=ret;
                                         DrawGrid1DblClick(DrawGrid1);
                                         end else//zabrat
                                         SpeedButton3Click(SpeedButton3);
                                 end else
                                 begin
-                                if can_cthrow(c2)=true then
-                                        begin      
+                                if can_cthrow(c2,ret)=true then
+                                        begin
                                         SpeedButton1Click(SpeedButton1);
-                                        for i:=c2.size-1 downto 0 do if reta[i]=1 then
-//                                        while can_cthrow(c2) do
+                                        for i:=0 to c2.size-1 do if reta[i]=1 then
                                                 begin
                                                 DrawGrid2.Col:=i;
                                                 DrawGrid2DblClick(DrawGrid2);
                                                 end;
 
                                         end else //otbit
-                                if can_cbeat(cg.a[cg.size-1],c2,ret)=true then
+                                if can_cbeat(c2.a[c2.size-1],cg,ret)=true then
                                         begin
                                         SpeedButton2Click(SpeedButton2);
                                         DrawGrid2.Col:=ret;
-                                        DrawGrid2DblClick(DrawGrid2);
                                         end else//sbros kozyrey
                                 if can_cthrow2(c2,ret)=true then
                                         begin
-                                        SpeedButton1Click(SpeedButton1);
                                         DrawGrid2.Col:=ret;
                                         DrawGrid2DblClick(DrawGrid2);
                                         end else//zabrat
@@ -719,8 +688,8 @@ if comp[currig-1]=1 then
                         SpeedButton5Click(SpeedButton5);
                         end;
                 end;
-        //showmessage('Компьютер сделал ход');
-        end //else showmessage('Сейчас ходите вы');
+        end;
+showmessage('0-');
 end;
 
 procedure TForm1.CheckBox1Click(Sender: TObject);
@@ -736,43 +705,6 @@ else comp[1]:=0;
 end;
 
 procedure TForm1.SpeedButton8Click(Sender: TObject);
-begin
-//timer1timer(timer1);
-Timer1.Enabled:=not Timer1.Enabled;
-if Timer1.Enabled then SpeedButton8.Caption:='Выключить таймер'
-else SpeedButton8.Caption:='Включить таймер'
-end;
-
-procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
-bmp.Free;
-end;
-
-procedure TForm1.check_win;
-var Action: TCloseAction;
-begin
-if (c1.size=0) and (c.size=0) then
-        begin
-        Timer1.Enabled:=false;
-        showmessage('Выиграл первый');
-        //FormClose(Form1,action);
-        //FormCreate(Form1);
-        end;
-if (c2.size=0) and (c.size=0) then
-        begin
-        Timer1.Enabled:=false;
-        showmessage('Выиграл второй');
-        //FormClose(Form1,action);
-        //FormCreate(Form1);
-        end;
-end;
-
-procedure TForm1.FormClick(Sender: TObject);
-begin
-showmessage(inttostr(drawgrid2.width));
-end;
-
-procedure TForm1.SpeedButton9Click(Sender: TObject);
 begin
 timer1timer(timer1);
 end;
